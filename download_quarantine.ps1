15c6cc12de8d3db4d9c32e40a6c4cdb06be0dd06
Daniel Card <dan.card@xservus.com>
2022-09-15 13:02:11 +0100

    Create download_quarantine.ps1

---
+++ download_quarantine.ps1
@@ -0,0 +1,24 @@
+# Crappy POC to show how to you dowload eml files from quarantine
+# mRr3b00t
+Install-Module -Name ExchangeOnlineManagement
+
+Set-ExecutionPolicy RemoteSigned
+
+Import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+$Quaratine = Get-QuarantineMessage
+$c = 0
+foreach($item in $Quaratine){
+$item.Identity
+$item.MessageId
+$c = $c + 1
+
+$file = "$c" + ".eml"
+
+$name = $item.Subject
+$data = Export-QuarantineMessage -Identity $item.Identity
+$bytes = [Convert]::FromBase64String($data.eml) | Out-File $file
+
+}
